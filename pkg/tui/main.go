903c6e0694b0f41602187fa656c1ef0d536d34a4
Erick Suarez <ericksuarezdev@gmail.com>
2022-08-17 18:39:22 -0500

    HElp done

--- pkg/tui/main.go
+++ pkg/tui/main.go
@@ -1,44 +1,178 @@
 package tui
 
 import (
-	ui "github.com/gizak/termui/v3"
-	"github.com/gizak/termui/v3/widgets"
+	"fmt"
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/lipgloss"
+	"os"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
+type keyMap struct {
+	Up    key.Binding
+	Down  key.Binding
+	Left  key.Binding
+	Right key.Binding
+	Help  key.Binding
+	Quit  key.Binding
+}
+
 type View struct {
+}
+
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Help, k.Quit}
+}
+
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Up, k.Down, k.Left, k.Right},
+		{k.Help, k.Quit},
+	}
+}
+
+var keys = keyMap{
+	Up: key.NewBinding(
+		key.WithKeys("up", "k"),
+		key.WithHelp("↑/k", "move up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down", "j"),
+		key.WithHelp("↓/j", "move down"),
+	),
+	Left: key.NewBinding(
+		key.WithKeys("left", "h"),
+		key.WithHelp("←/h", "move left"),
+	),
+	Right: key.NewBinding(
+		key.WithKeys("right", "l"),
+		key.WithHelp("→/l", "move right"),
+	),
+	Help: key.NewBinding(
+		key.WithKeys("?"),
+		key.WithHelp("?", "show help"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "esq", "ctrl+c"),
+		key.WithHelp("q", "quit"),
+	),
+}
+
+type model struct {
+	keys       keyMap
+	help       help.Model
+	inputStyle lipgloss.Style
+	lastKey    string
+	quitting   bool
+}
+
+func newModel() model {
+	return model{
+		keys:       keys,
+		help:       help.New(),
+		inputStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("#FF75B7")),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.help.Width = msg.Width
+
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.keys.Up):
+			m.lastKey = "↑"
+		case key.Matches(msg, m.keys.Down):
+			m.lastKey = "↓"
+		case key.Matches(msg, m.keys.Left):
+			m.lastKey = "←"
+		case key.Matches(msg, m.keys.Right):
+			m.lastKey = "→"
+		case key.Matches(msg, m.keys.Help):
+			m.help.ShowAll = !m.help.ShowAll
+		case key.Matches(msg, m.keys.Quit):
+			m.quitting = true
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.quitting {
+		return "Bye!\n"
+	}
+
+	var status string
+	if m.lastKey == "" {
+		status = "Waiting for input..."
+	} else {
+		status = "You choose:" + m.inputStyle.Render(m.lastKey)
+	}
+
+	helpView := m.help.View(m.keys)
+	height := 8 - strings.Count(status, "\n") - strings.Count(helpView, "\n")
+
+	return "\n" + status + strings.Repeat("\n", height) + helpView
 }
 
 func NewView() *View {
 	return &View{}
 }
 
+//func (v *View) Render() error {
+//	if err := ui.Init(); err != nil {
+//		return err
+//	}
+//	defer ui.Close()
+//
+//	p := widgets.NewParagraph()
+//	p.Text = "Here will be the menu"
+//	p.Title = "Menu"
+//	p.SetRect(0, 0, 50, 5)
+//
+//	grid := ui.NewGrid()
+//	termWidth, termHeight := ui.TerminalDimensions()
+//	grid.SetRect(0, 0, termWidth, termHeight)
+//
+//	grid.Set(
+//		ui.NewRow(1.0/3,
+//			ui.NewCol(2.0/3, p),
+//		),
+//	)
+//
+//	ui.Render(grid)
+//
+//	for e := range ui.PollEvents() {
+//		if e.Type == ui.KeyboardEvent {
+//			break
+//		}
+//	}
+//	return nil
+//}
+
 func (v *View) Render() error {
-	if err := ui.Init(); err != nil {
-		return err
-	}
-	defer ui.Close()
-
-	p := widgets.NewParagraph()
-	p.Text = "Here will be the menu"
-	p.Title = "Menu"
-	p.SetRect(0, 0, 50, 5)
-
-	grid := ui.NewGrid()
-	termWidth, termHeight := ui.TerminalDimensions()
-	grid.SetRect(0, 0, termWidth, termHeight)
-
-	grid.Set(
-		ui.NewRow(1.0/3,
-			ui.NewCol(2.0/3, p),
-		),
-	)
-
-	ui.Render(grid)
-
-	for e := range ui.PollEvents() {
-		if e.Type == ui.KeyboardEvent {
-			break
+	if os.Getenv("HELP_DEBUG") != "" {
+		if f, err := tea.LogToFile("debug.log", "help"); err != nil {
+			fmt.Println("Couldn't open a file for logging:", err)
+			os.Exit(1)
+		} else {
+			defer f.Close()
 		}
 	}
+
+	if err := tea.NewProgram(newModel()).Start(); err != nil {
+		fmt.Printf("Could not start program :(\n%v\n", err)
+		os.Exit(1)
+	}
+
 	return nil
 }
